259141e4a30125aeeda38e10ed447d0090a37266
kevinramirez23 <87163557+kevinramirez23@users.noreply.github.com>
2021-09-15 14:03:02 -0500

    Add files via upload

---
+++ hashlips_art_engine-1.0.2/src/config.js
@@ -0,0 +1,42 @@
+const description =
+  "This is the description of your NFT project, remember to replace this";
+const baseUri = "https://hashlips/nft";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 20,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+];
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+};
