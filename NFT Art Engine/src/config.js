cb5fef6e3d05e7b832ebe64c1818f6a1bf23bf46
kevinramirez23 <87163557+kevinramirez23@users.noreply.github.com>
2021-09-18 20:59:26 -0500

    Add files via upload

---
+++ NFT Art Engine/src/config.js
@@ -0,0 +1,43 @@
+const { MODE } = require("./blendMode.js");
+const description =
+  "This is the description of your NFT project, remember to replace this";
+const baseUri = "ipfs://QmNfPMWLPTEbFpBtPFy4wkYEHRVWcz8dzjziTcPbebzF53";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 20,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+];
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+};
