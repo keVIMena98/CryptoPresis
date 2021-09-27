947cafa64dd467dcccb751d2bfd5485a09e4a805
kevinramirez23 <87163557+kevinramirez23@users.noreply.github.com>
2021-09-27 01:58:58 -0500

    added frontend dapp folder to webpage

---
+++ webpage/frontend-base-dapp/src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { connect } from "./redux/blockchain/blockchainActions";
+import { fetchData } from "./redux/data/dataActions";
+import * as s from "./styles/globalStyles";
+import styled from "styled-components";
+import { create } from "ipfs-http-client";
+
+export const StyledButton = styled.button`
+  padding: 8px;
+`;
+
+function App() {
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+
+  useEffect(() => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  }, [blockchain.smartContract, dispatch]);
+
+  return (
+    <s.Screen>
+      {blockchain.account === "" || blockchain.smartContract === null ? (
+        <s.Container flex={1} ai={"center"} jc={"center"}>
+          <s.TextTitle>Connect to the Blockchain</s.TextTitle>
+          <s.SpacerSmall />
+          <StyledButton
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch(connect());
+            }}
+          >
+            CONNECT
+          </StyledButton>
+          <s.SpacerSmall />
+          {blockchain.errorMsg !== "" ? (
+            <s.TextDescription>{blockchain.errorMsg}</s.TextDescription>
+          ) : null}
+        </s.Container>
+      ) : (
+        <s.Container flex={1} ai={"center"} style={{ padding: 24 }}>
+          <s.TextTitle style={{ textAlign: "center" }}>
+            Name: {data.name}.
+          </s.TextTitle>
+        </s.Container>
+      )}
+    </s.Screen>
+  );
+}
+
+export default App;
