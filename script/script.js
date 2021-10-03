b1b13377989b4beb76451819d12481e9722cebec
mikem <77694074+mikejm15@users.noreply.github.com>
2021-10-03 14:01:23 -0500

    adding automatic image slider function

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+let counter = 1;
+
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 3){
+        counter = 1;
+    }
+}, 3000);
+
