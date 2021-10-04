5043da3132fe774ae821e0a3dbf1b9d174ea48f0
kevinramirez23 <87163557+kevinramirez23@users.noreply.github.com>
2021-10-03 22:49:04 -0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,5 +7,26 @@ setInterval(function(){
     if(counter > 3){
         counter = 1;
     }
+    document.getElementById
 }, 5000);
 
+document.addEventListener('click', e => {
+
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
+    if(!isDropdownButton && e.target.closest('[data-dropdown]') != null){
+        return;
+    }
+
+    let currentDropdown;
+    if(isDropdownButton){
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown =>{
+        if(dropdown === currentDropdown){
+            return;
+        }
+        dropdown.classList.remove('active');
+    })
+});
