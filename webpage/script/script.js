889fd2c733b01a72cc6936c5f604bc6f75c72213
mikem <77694074+mikejm15@users.noreply.github.com>
2021-10-15 22:22:39 -0500

    minor changes to image slider and added files

--- webpage/script/script.js
+++ webpage/script/script.js
@@ -4,7 +4,7 @@ let counter = 1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
     counter++;
-    if(counter > 3){
+    if(counter > 7){
         counter = 1;
     }
     document.getElementById
