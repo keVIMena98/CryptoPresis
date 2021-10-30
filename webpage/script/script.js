0907b1d870dd99691321eb7dbc98380446d4214d
Mike <77694074+mikejm15@users.noreply.github.com>
2021-10-29 20:10:31 -0500

    adding rest of legendaries to slider

--- webpage/script/script.js
+++ webpage/script/script.js
@@ -4,7 +4,7 @@ let counter = 1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
     counter++;
-    if(counter > 6){
+    if(counter > 10){
         counter = 1;
     }
     document.getElementById
