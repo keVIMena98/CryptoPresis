a59150a62cef90bb32467be33eb666081a1ba384
ackermann721 <kevin2014joel@gmail.com>
2022-12-29 13:11:04 -0500

    attempt to fix breaking changes

--- webpage-v2/webpack.config.js
+++ webpage-v2/webpack.config.js
@@ -1,17 +1,15 @@
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
-    resolve: {
-        extensions: [".tsx", ".ts", ".js", ".json"],
-        fallback: { 
-            "os": require.resolve('os-browserify/browser'),
-            "http": require.resolve('stream-http'),
-            "https": require.resolve('https-browserify'),
-            "stream": require.resolve('stream-browserify'),
-            "crypto": require.resolve('crypto-browserify')
-        }
-    }, 
-    plugins: [
-        new NodePolyfillPlugin()
-    ]
-}
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.json'],
+		fallback: {
+			os: require.resolve('os-browserify/browser'),
+			http: require.resolve('stream-http'),
+			https: require.resolve('https-browserify'),
+			stream: require.resolve('stream-browserify'),
+			crypto: require.resolve('crypto-browserify'),
+		},
+	},
+	plugins: [new NodePolyfillPlugin()],
+};
